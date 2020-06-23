e59a640fd4bf81b8456c535b08124c0d77551195
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-06-22 23:43:23 -0400

    Feature: Added math operations and imported math

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,9 +1,26 @@
+import math
+from CsvReader import CsvReader
+
+
 def addition(a, b):
     return a + b
 
 def subtraction(a, b):
     return a - b
 
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+def square(a):
+    b = int(a) * int(a)
+    return b
+
+def math_squareroot(a):
+    b = math.sqrt(a)
+    return b
 
 class Calculator:
     result = 0
@@ -17,4 +34,20 @@ class Calculator:
 
     def subtract(self, a, b):
         self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def square(self, a):
+        self.result = square(self, a)
+        return self.result
+
+    def squareroot(self, a):
+        self.result = math_squareroot(self, a)
         return self.result
